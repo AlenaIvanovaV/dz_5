e2725cb6f6444fd18a3cf00d4e3478b4d7a066c1
ivanova_av10 <angel40995@inbox.ru>
2022-12-13 19:38:02 +0300

    поправила замечание

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,32 +1,30 @@
 import os
+
+from selene import have
 from selene.support.shared import browser
-from selene import be, have
-from selene import command
 
 
 def test_fill_form(open_browser):
     browser.open('/automation-practice-form')
-    browser.element('[id="firstName"]').type('Алена')
-    browser.element('[id="lastName"]').type('Иванова')
-    browser.element('[id="userEmail"]').type('angel@mail.com')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('[id="userNumber"]').type('892777712345')
-    browser.element('[id="dateOfBirthInput"]').click()
+    browser.execute_script('document.querySelector(".body-height").style.transform = "scale(.75)"')
+    browser.element('#firstName').type('Алена')
+    browser.element('#lastName').type('Иванова')
+    browser.element('#userEmail').type('angel@mail.com')
+    browser.element('[for="gender-radio-2"]').click()
+    browser.element('#userNumber').type('892777712345')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('.react-datepicker__year-select>[value="1900"]').click()
     browser.element('.react-datepicker__month-select>[value="11"]').click()
-    browser.element('.react-datepicker__year-select>[value="1996"]').click()
-    browser.element('.react-datepicker__day--007').click()
-    browser.element('[id="subjectsInput"]').type("Maths").press_enter()
-    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element('.react-datepicker__day--013').click()
+    browser.element('#subjectsInput').type('comp').press_enter()
+    browser.element('[for="hobbies-checkbox-3"]').click()
     browser.element('#uploadPicture').set_value(
         os.path.abspath(os.path.join(os.path.dirname(__file__), '../files/picture.png')))
-    browser.element("#state").click()
-    browser.element("#react-select-3-input").type('Uttar Pradesh').press_enter()
-    browser.element("#city").click()
-    browser.element("#react-select-4-input").type('Agra').press_enter()
-
-    browser.element('[id="currentAddress"]').type('г.Москва ул Ленина 3')
-    browser.element('[id="subjectsInput"]').type("Maths").press_enter()
-    browser.element('#submit').perform(command.js.click)
+    browser.element('#currentAddress').type('г.Москва ул Ленина 3')
+    browser.element('#react-select-3-input').type('e').press_enter()
+    browser.element('#react-select-4-input').type('e').press_enter()
+    browser.element('#submit').press_enter()
     browser.element('.table').should(have.text(
         'Алена Иванова' and
         'angel@mail.com' and
@@ -37,5 +35,5 @@ def test_fill_form(open_browser):
         'Reading' and
         'picture.png' and
         'г.Москва ул Ленина 3' and
-        'Uttar Pradesh Agra'
+        'Uttar Pradesh Merrut'
     ))
